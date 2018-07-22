477352a96c68c9ed5acce78d7db542a573eb3a14
Akshay Bodhare <abodhare1234@gmail.com>
2018-07-22 17:30:25 +0530

    Added dummy code for get_data.py

--- get_data.py
+++ get_data.py
@@ -1,1 +1,8 @@
 """Use alpha vantage API to get stock data."""
+
+from alpha_vantage.timeseries import TimeSeries
+
+ALPHAVANTAGE_API_KEY = input("Enter you key")
+
+ts = TimeSeries(key=ALPHAVANTAGE_API_KEY)
+data, metadata = ts.get_intraday('GOOGL')
