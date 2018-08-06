62094c4d4961a97e84fa9923d88b8f38d1188ee8
Akshay Bodhare <abodhare1234@gmail.com>
2018-08-06 19:55:15 +0530

    Changed a lot. correct tickers.csv. Improve design of input

--- dashboard.py
+++ dashboard.py
@@ -1,61 +1,105 @@
 import dash
-from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 
+import colorlover as cl
+import datetime as dt
+import flask
+import os
+import pandas as pd
 from get_data import get_intraday_data
-from datetime import datetime as dt
+import time
 
 app = dash.Dash('stock-tickers')
+server = app.server
+
+colorscale = cl.scales['9']['qual']['Paired']
+
+df_symbol = pd.read_csv('tickers.csv')
 
 app.layout = html.Div([
     html.Div([
-        dcc.Input(id='my-id', value='API_KEY', type='text'),
-        html.Div(id='intermediate-value', style={'display': 'none'}),
-        dcc.Dropdown(
-            id='my-dropdown',
-            options=[
-                {'label': 'Coke', 'value': 'COKE'},
-                {'label': 'Tesla', 'value': 'TSLA'},
-                {'label': 'Apple', 'value': 'AAPL'}
-            ],
-            value='COKE'
-        ),
-        dcc.Dropdown(
-            id='my-dropdown1',
-            options=[
-                {'label': '1 minute', 'value': '1min'},
-                {'label': '5 minutes', 'value': '5min'},
-                {'label': '15 minutes', 'value': '15min'},
-                {'label': '30 minutes', 'value': '30min'},
-                {'label': '60 minutes', 'value': '60min'},
-            ],
-            value='60min'
-        )
-    ], style={'columnCount': 3}),
-    dcc.Graph(id='my-graph')
-], style={'rowCount': 2}) 
+        html.H2('Stock Portfolio',
+                style={'display': 'inline',
+                       'float': 'left',
+                       'font-size': '2.65em',
+                       'margin-left': '7px',
+                       'font-weight': 'bolder',
+                       'font-family': 'Product Sans',
+                       'color': "rgba(117, 117, 117, 0.95)",
+                       'margin-top': '20px',
+                       'margin-bottom': '0'
+                       }),
+    ]),
+    # Get API_KEY from user.
+    dcc.Input(id='API_KEY', value='enter API_KEY here', type='text'),
+    dcc.Dropdown(
+        id='stock-ticker-input',
+        options=[{'label': s[0], 'value': str(s[1])}
+                 for s in zip(df_symbol.Company, df_symbol.Symbol)],
+        value=['YHOO', 'GOOGL'],
+        multi=True
+    ),
+    html.Div(id='graphs')
+], className="container")
 
-@app.callback(Output('intermediate-value', 'children'), [Input('my-id', 'value')])
-def update_output_div(input_value):
-    API_KEY = input_value
-    return API_KEY
+def bbands(price, window_size=10, num_of_std=5):
+    rolling_mean = price.rolling(window=window_size).mean()
+    rolling_std  = price.rolling(window=window_size).std()
+    upper_band = rolling_mean + (rolling_std*num_of_std)
+    lower_band = rolling_mean - (rolling_std*num_of_std)
+    return rolling_mean, upper_band, lower_band
 
-@app.callback(Output('my-graph', 'figure'), [Input('my-dropdown', 'value'),
-                                    Input('intermediate-value', 'children'),
-                                    Input('my-dropdown1', 'value')])
-def update_graph(selected_dropdown_value, input_key, interval):
-    API_KEY = input_key
-    df = get_intraday_data(API_KEY, selected_dropdown_value, interval)
-    return {
-        'data': [{
-            'x': list(df.index),
-            'y': df['4. close']
-        }],
-        'layout': {'margin': {'l': 40, 'r': 10, 't': 20, 'b': 30},
-                   'title': '{} data of {}'.format(interval,
-                                                    selected_dropdown_value)}
-    }
+@app.callback(
+    dash.dependencies.Output('graphs','children'),
+    [dash.dependencies.Input('stock-ticker-input', 'value'),
+     dash.dependencies.Input('API_KEY', 'value')])
+def update_graph(tickers, API_KEY):
+    graphs = []
+    for i, ticker in enumerate(tickers):
+        try:
+            df = get_intraday_data(API_KEY, ticker, interval='60min')
+        except:
+            graphs.append(html.H3(
+                'Data is not available for {}, please retry later.'.format(ticker),
+                style={'marginTop': 20, 'marginBottom': 20}
+            ))
+            continue
+
+        candlestick = {
+            'x': df.index,
+            'open': df['1. open'],
+            'high': df['2. high'],
+            'low': df['3. low'],
+            'close': df['4. close'],
+            'type': 'candlestick',
+            'name': ticker,
+            'legendgroup': ticker,
+            'increasing': {'line': {'color': colorscale[0]}},
+            'decreasing': {'line': {'color': colorscale[1]}}
+        }
+        bb_bands = bbands(df['4. close'])
+        bollinger_traces = [{
+            'x': df.index, 'y': y,
+            'type': 'scatter', 'mode': 'lines',
+            'line': {'width': 1, 'color': colorscale[(i*2) % len(colorscale)]},
+            'hoverinfo': 'none',
+            'legendgroup': ticker,
+            'showlegend': True if i == 0 else False,
+            'name': '{} - bollinger bands'.format(ticker)
+        } for i, y in enumerate(bb_bands)]
+        graphs.append(dcc.Graph(
+            id=ticker,
+            figure={
+                'data': [candlestick] + bollinger_traces,
+                'layout': {
+                    'margin': {'b': 0, 'r': 10, 'l': 60, 't': 0},
+                    'legend': {'x': 0}
+                }
+            }
+        ))
+
+    return graphs
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True)
--- get_data.py
+++ get_data.py
@@ -8,3 +8,9 @@ def get_intraday_data(API_KEY, ticker, interval):
     data, metadata = ts.get_intraday(symbol=ticker, 
                                     interval=interval, outputsize='full')
     return data
+
+def get_daily(API_KEY, ticker):
+    """get dataframe of bbands for the given stocks."""
+    ts = TimeSeries(API_KEY, output_format='pandas')
+    data, meta_data = ts.get_daily(symbol=ticker, outputsize='full')
+    return data
